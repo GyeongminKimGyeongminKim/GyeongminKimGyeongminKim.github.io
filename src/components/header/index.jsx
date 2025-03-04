86461cad4347eea117b306f6a6574e701abdadba
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-03-05 08:36:32 +0900

    feat: 이메일, 전화번호 작성

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -10,8 +10,8 @@ const classes = {
   contentWrapper: 'flex-none pt-6 md:pt-1 md:flex-1 md:pl-20',
   name: 'text-5xl text-gray-900 font-bold leading-tight hover:text-black',
   description: 'text-gray-600',
-  list: 'mt-6 uppercase tracking-wider',
-  item: 'inline list-none pr-4',
+  list: 'mt-6 tracking-wider',
+  item: 'inline list-none pr-4 uppercase',
   link: 'inline-block py-2 font-semibold text-xs text-gray-600 hover:text-black',
 };
 
@@ -21,12 +21,12 @@ const Header = ({ metadata = {}, noBlog = false }) => {
   const linkedin = get(metadata, 'linkedin', false);
 
   return (
-    <div className={classes.wrapper}>
+    <div className={classes.wrapper}style={{borderBottom: "8px solid rgba(179, 189, 68, 0.43)", }}>
       <div className={classes.imageWrapper}>
         <Link to="/">
           <StaticImage
             className={classes.image}
-            src="../../images/profile.jpg"
+            src="../../images/android_icon.png"
             alt={metadata.name}
             placeholder="dominantColor"
             layout="fullWidth"
@@ -39,7 +39,7 @@ const Header = ({ metadata = {}, noBlog = false }) => {
         </h1>
         <p className={classes.description}>{metadata.description}</p>
         <ul className={classes.list}>
-          {twitter && (
+          {/* {twitter && (
             <li className={classes.item}>
               <a
                 className={classes.link}
@@ -48,7 +48,7 @@ const Header = ({ metadata = {}, noBlog = false }) => {
                 Twitter
               </a>
             </li>
-          )}
+          )} */}
           {github && (
             <li className={classes.item}>
               <a className={classes.link} href={github}>
@@ -63,13 +63,23 @@ const Header = ({ metadata = {}, noBlog = false }) => {
               </a>
             </li>
           )}
-          {!noBlog && (
+          {/* {!noBlog && (
             <li className={classes.item}>
               <Link className={classes.link} to="/blog">
                 Blog
               </Link>
             </li>
-          )}
+          )} */}
+          <li className={`${classes.item} lowercase`}>
+            <a className={classes.link}>
+              kiddevill@naver.com
+            </a>
+          </li>
+          <li className={classes.item}>
+            <a className={classes.link}>
+              010-4165-0810
+            </a>
+          </li>
         </ul>
       </div>
     </div>
